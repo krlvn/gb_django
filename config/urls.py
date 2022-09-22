6c0e9074fb7e5498cd1bbbf5859b65614b337340
krlvn <krlvn39@gmail.com>
2022-09-22 14:09:21 +0200

    Добавил namespace в urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
+from mainapp.apps import MainappConfig
+
 urlpatterns = [
     path('', RedirectView.as_view(url='mainapp/')),
-    path("mainapp/", include("mainapp.urls")),
-    path("admin/", admin.site.urls),
+    path('mainapp/', include('mainapp.urls', namespace=MainappConfig.name)),
+    path('admin/', admin.site.urls),
 ]
