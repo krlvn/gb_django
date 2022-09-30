d9ef12e7690c3efea69c314effc3a8f5c4ec9cee
krlvn <krlvn39@gmail.com>
2022-09-30 15:12:47 +0200

    Роутеры для приложения authapp.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,7 +7,6 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main'),
-    path('login/', LoginPageView.as_view(), name='login'),
     path('doc_site/', DocSitePageView.as_view(), name='doc_site'),
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
     path('news/', NewsPageView.as_view(), name='news'),
