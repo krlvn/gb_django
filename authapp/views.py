8cc4cffbde39c649db4a433442e980ad0350f91c
krlvn <krlvn39@gmail.com>
2022-09-30 15:03:53 +0200

    Контроллер и шаблон страницы редактирования профиля.

--- authapp/views.py
+++ authapp/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
@@ -7,6 +8,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
 from authapp import models
+
 
 class CustomLoginView(LoginView):
     def form_valid(self, form):
@@ -26,7 +28,6 @@ class CustomLoginView(LoginView):
                 mark_safe(f'Something goes wrong:<br>{msg}'),
             )
         return self.render_to_response(self.get_context_data(form=form))
-
 
 
 class RegisterView(TemplateView):
@@ -66,3 +67,34 @@ class RegisterView(TemplateView):
                 mark_safe(f'Something goes wrong:<br>{exp}'),
             )
             return HttpResponseRedirect(reverse_lazy('authapp:register'))
+
+
+class ProfileEditView(LoginRequiredMixin, TemplateView):
+    template_name = 'registration/profile_edit.html'
+    login_url = reverse_lazy('authapp:login')
+
+    def post(self, request, *args, **kwargs):
+        try:
+            if request.POST.get('username'):
+                request.user.username = request.POST.get('username')
+            if request.POST.get('first_name'):
+                request.user.first_name = request.POST.get('first_name')
+            if request.POST.get('last_name'):
+                request.user.last_name = request.POST.get('last_name')
+            if request.POST.get('age'):
+                request.user.age = request.POST.get('age')
+            if request.POST.get('email'):
+                request.user.email = request.POST.get('email')
+            if request.FILES.get('avatar'):
+                if request.user.avatar and os.path.exists(request.user.avatar.path):
+                    os.remove(request.user.avatar.path)
+                request.user.avatar = request.FILES.get('avatar')
+            request.user.save()
+            messages.add_message(request, messages.INFO, _('Изменения сохранены!'))
+        except Exception as exp:
+            messages.add_message(
+                request,
+                messages.WARNING,
+                mark_safe(f'''Something goes wrong:<br>{exp}'''),
+            )
+        return HttpResponseRedirect(reverse_lazy('authapp:profile_edit'))
