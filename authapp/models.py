37225d7c88c37606ff56392ec33980be50946c53
krlvn <krlvn39@gmail.com>
2022-09-30 12:19:36 +0200

    Создал новую модель пользователя

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,88 @@
-from django.db import models
+from pathlib import Path
+from time import time
 
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.core.mail import send_mail
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+def users_avatars_path(instance, filename):
+    '''
+    Определяет путь для хранения аватарок
+    MEDIA_ROOT / user_<username> / avatars / <filename>
+    '''
+    num = int(time() * 1000)
+    suff = Path(filename).suffix
+    return f'''user_{instance.username}/avatars/pic_{num}{suff}'''
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username_validator = ASCIIUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Допускается не более 150 символов. Буквы, цифры и @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={'unique': _('Пользователь с таким именем уже зарегистрирован.'),},
+    )
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    avatar = models.ImageField(upload_to=users_avatars_path, blank=True, null=True)
+    email = models.CharField(
+        _('email address'),
+        max_length=256,
+        unique=True,
+        error_messages={'unique': _('Пользователь с таким email уже зарегистрирован.'),},
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Определяет, что пользователь является администратором.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_('Определяет, что пользователь активен или удалён.'),
+    )
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email',]
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def clean(self):
+        """
+        Нормализует email-адрес.
+        """
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Возвращяем строку с имем и фамилией разделённые проблом.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """
+        Возвращает имя пользователя (без фамилии).
+        """
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Отправляет email пользователю.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
