86e3f4203c5ba0e91c3a446d90958f869e1e6c7e
krlvn <krlvn39@gmail.com>
2022-10-03 13:21:05 +0200

    Зарегистрировал модель CustomUser

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from authapp import models as authapp_models
+@admin.register(authapp_models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'is_active', 'date_joined']
+    ordering = ['-date_joined']
