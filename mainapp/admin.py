5f691b89b92a8e3c1533fead1325b2095ed45e21
krlvn <krlvn39@gmail.com>
2022-10-03 12:34:48 +0200

    Зарегистрировал модель Lesson

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from mainapp import models as mainapp_models
 
 @admin.register(mainapp_models.News)
 class NewsAdmin(admin.ModelAdmin):
     search_fields = ['title', 'preambule', 'body']
+
+@admin.register(mainapp_models.Lessons)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_course_name', 'num', 'title', 'deleted']
+    ordering = ['-course__name', '-num']
+    list_per_page = 5
+    list_filter = ['course', 'create_date', 'deleted']
+    actions = ['mark_deleted']
+    search_fields = ['title', 'description']
+
+    def get_course_name(self, obj):
+        return obj.course.name
+    get_course_name.short_description = _('Course')
+
+    def mark_deleted(self, request, queryset):
+        queryset.update(deleted=True)
+    mark_deleted.short_description = _('Mark deleted')
