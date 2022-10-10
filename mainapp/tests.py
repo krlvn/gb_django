3368311f2d1fdc41c6eec3a6dd9b71fe0874f009
krlvn <krlvn39@gmail.com>
2022-10-10 22:04:13 +0200

    Тесты недоступности CRUD-операций для неаутентифицированного пользователя объектов новостей

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -79,3 +79,63 @@ class TestNewsPageWithAuth(TestCase):
         self.client_with_auth.post(path)
         news_obj.refresh_from_db()
         self.assertTrue(news_obj.deleted)
+
+
+class TestNewsPageNotAuth(TestCase):
+    fixtures = (
+        'mainapp/fixtures/001_news.json',
+    )
+
+    def test_open_page_create(self):
+        path = reverse('mainapp:news_create')
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+
+    def test_create(self):
+        counter_before = News.objects.count()
+        path = reverse('mainapp:news_create')
+        self.client.post(
+            path,
+            {
+                'title': 'Тest news title',
+                'preambule': 'Test news preambule',
+                'body': 'Test news body',
+            },
+        )
+        self.assertGreater(News.objects.count(), counter_before)
+
+    def test_open_page_update(self):
+        news_obj = News.objects.first()
+        path = reverse('mainapp:news_update', args=[news_obj.pk])
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+
+    def test_update(self):
+        new_title = 'New title of news'
+        news_obj = News.objects.first()
+        self.assertNotEqual(news_obj.title, new_title)
+        path = reverse('mainapp:news_update', args=[news_obj.pk])
+        result = self.client.post(
+            path,
+            {
+                'title': new_title,
+                'preambule': news_obj.preambule,
+                'body': news_obj.body,
+            },
+        )
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+        news_obj.refresh_from_db()
+        self.assertEqual(news_obj.title, new_title)
+
+    def test_open_page_delete(self):
+        news_obj = News.objects.first()
+        path = reverse('mainapp:news_delete', args=[news_obj.pk])
+        result = self.client.post(path)
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+
+    def test_delete(self):
+        news_obj = News.objects.first()
+        path = reverse('mainapp:news_delete', args=[news_obj.pk])
+        self.client.post(path)
+        news_obj.refresh_from_db()
+        self.assertTrue(news_obj.deleted)
